fc84c4143d05c3e3e81e30bac130b33eb5cd69d0
Rafael Mauricio <rafael_mauricio@hotmail.com>
2023-10-05 15:55:42 -0300

    botao remover produto do carrinho ok

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Objeto dados Usuario para encaminhar Wpp 
 let dadosUsuarioCep = {
   localidade: ``,
   bairro: ``,
@@ -5,6 +6,7 @@ let dadosUsuarioCep = {
   numero: ``
 }
 
+// configuração quando o modal estiver ativo
 const modalOn = () => {
   const modal = document.querySelector('.modal-off')
   const navbar = document.querySelector('.nav-bar')
@@ -15,6 +17,7 @@ const modalOn = () => {
   body.classList.toggle('overflow-hidden')
 }
 
+// Buscando o cep do usuario e imprimindo na tela
 const buscaCep = () => {
   const cepInput = document.querySelector('#cep').value;
   const resumoCep = document.querySelector('#conteudo');
@@ -39,7 +42,7 @@ const buscaCep = () => {
       resumoCep.innerHTML = `
         <p class="cidade">${localidade}</p>
         <p class="bairro">${bairro}</p>
-        <p class="rua">${logradouro} <span class="numero"><input class='input-style pequeno' placeholder="Nº" type="number" id="numero"></span></p> 
+        <p class="rua">${logradouro} <span class="numero"><input class='input-style pequeno' placeholder="Nº" type="number" id="numero"></span></p>
       `;
 
       document.querySelector('#numero').addEventListener('input', pegarNumeroCasa)
@@ -50,11 +53,17 @@ const buscaCep = () => {
     });
 }
 
+// pegando o numero da casa do usuario e passando esse valor para o objeto dadosUsuarioCep
 const pegarNumeroCasa = (event) => {
   dadosUsuarioCep.numero = event.target.value
 }
 
+// Finalizando o pedido imprimindo um alert e falta encaminhar para o wpp
 const finalizarPedido = () => {
+  if(dadosUsuarioCep.numero == 0) {
+    alert('Preencha o número da sua residencia!')
+    return
+  }
   alert(`
   Pedido Finalizado com Sucesso!
   =========================================
@@ -67,12 +76,65 @@ const finalizarPedido = () => {
   `)
 }
 
+// Atualizando o valor do subtotal e resumo
+const atualizarValor = () => {
+  let subTotal = 0
+  const precoProduto = document.querySelectorAll('.card-produto .preco .preco-produto')
+  for(var i = 0; i < precoProduto.length; i++) {
+    const preco = precoProduto[i].innerText.replace('R$', '').replace(',','.')
+    const quantidade = precoProduto[i].parentElement.parentElement.querySelector('.quantidade .form .quantidade-produto').value
+  
+    subTotal += preco * quantidade
+  }
+  document.querySelector('.subtotal span').innerHTML = `R$ ${subTotal.toFixed(2).replace('.',',')}`
+  document.querySelector('.valor-produtos span').innerHTML = `R$ ${subTotal.toFixed(2).replace('.',',')}`
+}
+atualizarValor()
 
+// Removendo produto e atualizando o valor
+const removerProduto = (event) => {
+  event.target.parentElement.parentElement.remove()
+  atualizarValor()
+}
+
+// Incrementado + 1 e atualizando o valor
+const incrementarProduto = (event) => {
+  event.target.parentElement.querySelector('.quantidade-produto').value ++
+  atualizarValor()
+}
+
+// Decrementando - 1 e atualizando o valor
+const decrementoProduto = (event) => {
+  event.target.parentElement.querySelector('.quantidade-produto').value --
+  atualizarValor()
+}
+
+// BOTAO DECREMENTO  -  Chamada dos objetos via DOM
+const botaoDecrementoProduto = document.querySelectorAll('.menos')
+for(var i = 0; i < botaoDecrementoProduto.length; i++) {
+  botaoDecrementoProduto[i].addEventListener('click', decrementoProduto)
+}
+
+// BOTAO INCREMENTO  -  Chamada dos objetos via DOM
+const botaoIncrementarProduto = document.querySelectorAll('.mais')
+for(var i = 0; i < botaoIncrementarProduto.length; i++) {
+  botaoIncrementarProduto[i].addEventListener('click', incrementarProduto)
+}
+
+// BOTAO REMOVER PRODUTO  -  Chamada dos objetos via DOM
+const botaoRemoverProduto = document.querySelectorAll('.remover-produto')
+for(var i = 0; i < botaoRemoverProduto.length; i++) {
+  botaoRemoverProduto[i].addEventListener('click', removerProduto)
+}
+
+// BOTAO CARRINHO DE COMPRA  -  Chamada dos objetos via DOM
 const carrinhoCompra = document.querySelector('.carrinho-compras')
 carrinhoCompra.addEventListener('click', modalOn)
 
+// BOTAO BUSCA CEP  -  Chamada dos objetos via DOM
 const botaoCep = document.querySelector('#botao-cep')
 botaoCep.addEventListener('click', buscaCep)
 
+// BOTAO FINALIZAR PEDIDO  -  Chamada dos objetos via DOM
 const botaoFinalizarPedido = document.querySelector('#finalizar-pedido')
 botaoFinalizarPedido.addEventListener('click', finalizarPedido)
